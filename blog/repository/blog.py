bc49598c3e3f1db96f98b2bd2c754e3ff67663bf
abubakkar-js-dev <mdabubakkars182@gmail.com>
2026-03-26 08:21:42 +0600

    update: refactor blog route and repo

---
+++ blog/repository/blog.py
@@ -0,0 +1,63 @@
+from fastapi import status,HTTPException
+from sqlalchemy.orm import Session
+from .. import models
+
+
+def get_all(db: Session):
+    blogs = db.query(models.Blog).all()
+
+    return blogs
+
+
+def create(request, db: Session):
+    new_blog = models.Blog(
+        title=request.title,
+        description=request.description,
+        published=request.published,
+        user_id=1
+    )
+    db.add(new_blog)
+    db.commit()
+    db.refresh(new_blog)
+    return new_blog
+
+
+def show(id,response,db: Session):
+    blog = db.query(models.Blog).filter(models.Blog.id == id).first()
+
+    if not blog:
+        response.status_code = status.HTTP_404_NOT_FOUND
+        return {'details': f'Blog with {id} is not available'}
+
+    return blog
+
+
+
+def destroy(id, db: Session):
+    blog = db.query(models.Blog).filter(models.Blog.id == id)
+
+    if not blog:
+        raise HTTPException(f'Blog with {id} not found')
+
+    blog.delete(synchronize_session=False)
+    db.commit()
+
+    return {'success': True}
+
+
+
+def update(id, request, db: Session):
+    updated_blog = {
+        'title': request.title,
+        'description': request.description,
+        'published': request.published,
+    }
+    blog = db.query(models.Blog).filter(models.Blog.id == id)
+
+    if not blog:
+        raise HTTPException(f'Blog with {id} not found')
+
+    blog.update(updated_blog)
+    db.commit()
+
+    return {'message': f'Updated blog of id:{id} successfully'}
--- blog/routers/blog.py
+++ blog/routers/blog.py
@@ -2,6 +2,7 @@ from fastapi import APIRouter,Depends,status,Response,HTTPException
 from typing import List
 from sqlalchemy.orm import Session
 from .. import schemas,models,database
+from ..repository import blog
 
 
 router = APIRouter(
@@ -9,68 +10,32 @@ router = APIRouter(
     tags=['Blogs']
 )
 
+
+
 @router.get('/', status_code=status.HTTP_200_OK, response_model=List[schemas.ShowBlog])
 def all(db: Session = Depends(database.get_db)):
-    blogs = db.query(models.Blog).all()
-    
-    return blogs
+    return blog.get_all(db)
 
 
 @router.post('/', status_code=status.HTTP_201_CREATED)
 def create(request: schemas.Blog, db: Session = Depends(database.get_db)):
-    new_blog = models.Blog(
-        title=request.title,
-        description=request.description,
-        published=request.published,
-        user_id = 1
-    )
-    db.add(new_blog)
-    db.commit()
-    db.refresh(new_blog)
-    return new_blog
+    return blog.create(request,db)
 
 
 
 # Get single blog
 @router.get('/{id}', status_code=200, response_model=schemas.ShowBlog)
 def show(id, response: Response, db: Session = Depends(database.get_db)):
-    blog = db.query(models.Blog).filter(models.Blog.id == id).first()
-
-    if not blog:
-        response.status_code = status.HTTP_404_NOT_FOUND
-        return {'details': f'Blog with {id} is not available'}
-
-    return blog
+    return blog.show(id,response,db)
 
 
 
 
 @router.delete('/{id}', status_code=status.HTTP_200_OK)
 def delete(id, db: Session = Depends(database.get_db)):
-    blog = db.query(models.Blog).filter(models.Blog.id == id)
-
-    if not blog:
-        raise HTTPException(f'Blog with {id} not found')
-
-    blog.delete(synchronize_session=False)
-    db.commit()
-
-    return {'success': True}
+    return blog.destroy(id,db)
 
 
 @router.put('/{id}', status_code=status.HTTP_202_ACCEPTED)
 def update(id, request: schemas.Blog, db: Session = Depends(database.get_db)):
-    updated_blog = {
-        'title': request.title,
-        'description': request.description,
-        'published': request.published,
-    }
-    blog = db.query(models.Blog).filter(models.Blog.id == id)
-
-    if not blog:
-        raise HTTPException(f'Blog with {id} not found')
-
-    blog.update(updated_blog)
-    db.commit()
-
-    return {'message': f'Updated blog of id:{id} successfully'}
+    return blog.update(id,request,db)
